0ba99abd5453ef5e90e6b1b3a5c2c354668b1f3a
Dusan <masinacdt@gmail.com>
2019-02-06 17:11:28 +0100

    amazon edit

--- phpcurl.php
+++ phpcurl.php
@@ -1,9 +1,15 @@
 <?php
 
 $curl= curl_init();
+$search_string="movies 2015";
+
+$url="https://www.amazon.com/s/filled-keywords=";
 
 curl_setopt($curl, CURLOPT_URL, 'https://www.amazon.com');
 curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+https://images-na.ssl-images-amazon.com/images/I/51r+cYzFbKL._AC_US218_.jpg
+curl_setopt($curl,CURLOPT_RETURNTRANSFER,true);
+$result=curl_exec($curl);
 
 
  ?>
