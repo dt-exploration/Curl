4033189933ed2fb5436269839f98e3cc2b11fa3e
Dusan <masinacdt@gmail.com>
2019-02-07 01:14:09 +0100

    variable curl

--- phpcurl.php
+++ phpcurl.php
@@ -1,15 +1,26 @@
 <?php
 
-$curl= curl_init();
-$search_string="movies 2015";
+//$curl= curl_init();
+//$search_string="movies 2015";
+//$url="http://tuck.rs/cms/search.php?id=99&startfrom=0&keyword=hk&submit=Pretraga";
+//curl_setopt($curl, CURLOPT_URL, $url);
+//curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
 
-$url="https://www.amazon.com/s/filled-keywords=$search_string";
+//https://images-na.ssl-images-amazon.com/images/I/51r+cYzFbKL._AC_US218_.jpg
+//curl_setopt($curl,CURLOPT_RETURNTRANSFER,true);
+//curl_exec($curl);
+//echo $result;
+//curl_close($curl);
 
-curl_setopt($curl, CURLOPT_URL, 'https://www.amazon.com');
-curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-https://images-na.ssl-images-amazon.com/images/I/51r+cYzFbKL._AC_US218_.jpg
-curl_setopt($curl,CURLOPT_RETURNTRANSFER,true);
-$result=curl_exec($curl);
+$konekt=curl_init();
+$url="https://www.google.com";
+curl_setopt($konekt, CURLOPT_URL, $url);
+curl_setopt($konekt,CURLOPT_RETURNTRANSFER,1);
+
+$result=curl_exec($konekt);
+curl_close($konekt);
+var_dump($result);
 
 
- ?>
+
+?>
