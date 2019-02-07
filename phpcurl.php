6e72f9b6a0fd12841e931f4f9ff62f928d8f8631
Dusan <masinacdt@gmail.com>
2019-02-07 01:37:18 +0100

    http status code

--- phpcurl.php
+++ phpcurl.php
@@ -11,7 +11,7 @@
 //curl_exec($curl);
 //echo $result;
 //curl_close($curl);
-
+/*
 $konekt=curl_init();
 $url="https://www.google.com";
 curl_setopt($konekt, CURLOPT_URL, $url);
@@ -21,10 +21,20 @@ $file=fopen("dt.txt", "w");
 curl_setopt($konekt, CURLOPT_FILE, $file);
 curl_exec($konekt);
 curl_close($konekt);
-
 fclose($file);
+*/
+$url="https://www.google.com";
+$ch=curl_init();
 
+curl_setopt($ch,CURLOPT_URL,$url);
+curl_setopt($ch,CURLOPT_RETURNTRANSFER,TRUE);
+curl_setopt($ch,CURLOPT_HEADER,true);
+curl_setopt($ch,CURLOPT_NOBODY,true);
 
+$output=curl_exec($ch);
+$http_code=curl_getinfo($ch,CURLINFO_HTTP_CODE);
 
+curl_close($ch);
 
+echo "$http_code";
 ?>
