6d30e0705d3569808a9b14a224ba3d51f4acdc48
dt-exploration <47108307+dt-exploration@users.noreply.github.com>
2019-03-13 09:20:55 +0100

    comments deleted (not working amazon service)

--- phpcurl.php
+++ phpcurl.php
@@ -1,17 +1,5 @@
 <?php
 
-//$curl = curl_init();
-//$search_string="movies 2015";
-//$url="http://tuck.rs/cms/search.php?id=99&startfrom=0&keyword=hk&submit=Pretraga";
-//curl_setopt($curl, CURLOPT_URL, $url);
-//curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-
-//https://images-na.ssl-images-amazon.com/images/I/51r+cYzFbKL._AC_US218_.jpg
-//curl_setopt($curl,CURLOPT_RETURNTRANSFER,true);
-//curl_exec($curl);
-//echo $result;
-//curl_close($curl);
-/*
 $konekt=curl_init();
 $url="https://www.google.com";
 curl_setopt($konekt, CURLOPT_URL, $url);
@@ -23,7 +11,7 @@ curl_exec($konekt);
 curl_close($konekt);
 fclose($file);
 */
-$url=  "https://www.google.com";
+$url =  "https://www.google.com";
 $ch = curl_init();
 
 curl_setopt($ch, CURLOPT_URL,$url);
@@ -37,4 +25,5 @@ $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 curl_close($ch);
 
 echo "$http_code";
+
 ?>
